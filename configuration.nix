a529f1f8157c41edbaecbb88a28f2908951c725f
Timothy Klim <git@timothyklim.com>
2021-03-09 23:59:28 +0700

    Fix configuration (2)

--- configuration.nix
+++ configuration.nix
@@ -24,7 +24,7 @@ let
     ${cfg.extraConfig}
   '';
 in
-with pkgs.lib; with lib; mkIf cfg.enable {
+with lib; mkIf cfg.enable {
   boot.kernel.sysctl = {
     "kernel.shmall" = mkDefault 4294967296;
     "kernel.shmmax" = mkDefault 1073741824;
